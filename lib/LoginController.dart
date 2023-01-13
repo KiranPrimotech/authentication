0baff2f6faca91a702681a59d1015b7b6d2c4fe8
KiranPrimotech <kiran.yadav@primotech.com>
2023-01-13 11:52:43 +0530

    13/1/2023

--- lib/LoginController.dart
+++ lib/LoginController.dart
@@ -2,6 +2,12 @@ import 'package:authentication/feature/LoginMethod.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 
 class LoginController{
+  ///Available Methods
+  ///GoogleLogin()
+  ///AppleLoginServices()
+  ///FaceBookLogin()
+  ///
+  ///You can create custom Login by extending LoginMethod
 
 
   Future<User?> loginController({required LoginMethod method})  async{
