d31c1a2bc325935e829539c111540d27b917e957
KiranPrimotech <kiran.yadav@primotech.com>
2023-01-19 14:25:23 +0530

    19/1/2023/master

--- lib/feature/email_login.dart
+++ lib/feature/email_login.dart
@@ -2,79 +2,67 @@ import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
 
 class EmailLogin {
-
   /// Create a New user
-  Future<User?> createUserViaEmail({required String email,required String password, required Function(FirebaseAuthException e) onError}) async{
-    User? user ;
+  Future<User?> createUserViaEmail(
+      {required String email,
+      required String password,
+      required Function(FirebaseAuthException e) onError}) async {
+    User? user;
     debugPrint("create via email");
     try {
-
-      final credential = await FirebaseAuth.instance.createUserWithEmailAndPassword(
-          email: email,
-          password: password
-      );
+      final credential = await FirebaseAuth.instance
+          .createUserWithEmailAndPassword(email: email, password: password);
       user = credential.user;
-      if(user!=null){
-
+      if (user != null) {
         debugPrint("User Email --- ${credential.user}");
-
-      }
-      else{
+      } else {
         debugPrint("User Email --- ${credential.user}");
       }
     } on FirebaseAuthException catch (e) {
-
       onError(e);
       if (e.code == 'user-not-found') {
         debugPrint('No user found for that email.');
       } else if (e.code == 'wrong-password') {
         debugPrint('Wrong password provided for that user.');
       }
-    }
-    catch(e){
+    } catch (e) {
       debugPrint('Exception Create User $e');
-
-
     }
     return user;
   }
-
 
   /// Login User If Already Present in the database
-  Future<User?> loginViaEmail({required String email,required String password , required Function(FirebaseAuthException e) onError}) async{
-    User? user ;
+  Future<User?> loginViaEmail(
+      {required String email,
+      required String password,
+      required Function(FirebaseAuthException e) onError}) async {
+    User? user;
     debugPrint("login via email");
     try {
-
-      final credential = await FirebaseAuth.instance.signInWithEmailAndPassword(
-          email: email,
-          password: password
-      );
-     user = credential.user;
-      if(user!=null){
-
+      final credential = await FirebaseAuth.instance
+          .signInWithEmailAndPassword(email: email, password: password);
+      user = credential.user;
+      if (user != null) {
         debugPrint("User Email --- ${credential.user}");
-
-      }
-      else{
+      } else {
         debugPrint("User Email --- ${credential.user}");
       }
-
     } on FirebaseAuthException catch (e) {
-      onError(e);
-      // Error().authError(e);
-      // if (e.code == 'user-not-found') {
-      //    user = await createUserViaEmail(email: email,password: password);
-      //   debugPrint('No user found for that email.');
-      // } else if (e.code == 'wrong-password') {
-      //   debugPrint('Wrong password provided for that user.');
-      // }
-    }
-    catch(e){
+      if (e.code == 'user-not-found') {
+        user = await createUserViaEmail(
+            email: email,
+            password: password,
+            onError: (FirebaseAuthException e) {
+              onError(e);
+            });
+        debugPrint('No user found for that email.');
+      } else {
+        onError(e);
+        debugPrint('Wrong password provided for that user.');
+      }
+    } catch (e) {
       debugPrint('Exception Login  $e');
-
     }
     return user;
   }
-
 }
